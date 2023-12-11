85d8f33ad51863c500cb92b1d57a745c77e311b0
aamir <aamir.khan2478@gmail.com>
2023-12-11 17:31:03 +0500

    Add some endpoint routes for contract

---
+++ routes/contract.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+const router = express.Router();
+import { addContract } from "../controllers/contract.controller.js";
+import validatorMiddleware from "../middlewares/validator.middleware.js";
+import upload from "../middlewares/multer.middleware.js";
+import contractSchema from "../validator/contract.validator.js";
+
+router.post(
+  "/add",
+  upload.array("images", 10),
+  validatorMiddleware(contractSchema),
+  addContract
+);
+
+export default router;
