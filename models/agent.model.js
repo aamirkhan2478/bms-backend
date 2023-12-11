85ac80311980a4b8980e37469c77977ea186a7b5
aamir <aamir.khan2478@gmail.com>
2023-12-11 17:27:26 +0500

    Add agent schema for store data

---
+++ models/agent.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const agentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Agent = mongoose.model("Agent", agentSchema);
+
+export default Agent;
