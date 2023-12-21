d4f9583f867aa850b428a0d6524f7050fd39c96a
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-21 17:34:05 +0500

    Add showOwners route to owner.routes.js

--- src/routes/owner.routes.js
+++ src/routes/owner.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import {
   addOwner,
   showInventoriesWithSpecificOwners,
+  showOwners,
 } from "../controllers/owner.controller.js";
 import ownerTenantSchema from "../validator/owner_tenant.validator.js";
 import validatorMiddleware from "../middlewares/validator.middleware.js";
@@ -15,6 +16,7 @@ router
     validatorMiddleware(ownerTenantSchema),
     addOwner
   );
+router.route("/all").get(showOwners);
 router.route("/show-inventories").get(showInventoriesWithSpecificOwners);
 
 export default router;
