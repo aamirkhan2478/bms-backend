2b123151f569967dd38b900f716a467e744dcc19
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-21 16:33:49 +0500

    Add purchaseDate to sellInventory and create showInventories and showOwnersWithSpecificInventories

--- src/controllers/inventory.controller.js
+++ src/controllers/inventory.controller.js
@@ -26,13 +26,12 @@ export const addInventory = asyncHandler(async (req, res) => {
 // @desc    Sell inventory
 // @access  Private
 export const sellInventory = asyncHandler(async (req, res) => {
-  const { owners, inventories } = req.body;
+  const { owners, inventories, purchaseDate } = req.body;
   const status = "sold";
 
   const inventoriesArray = mappingArray(inventories);
   const ownersArray = mappingArray(owners);
 
-  // update inventory id in owner model
   for (const owner of ownersArray) {
     await Owner.findByIdAndUpdate(
       owner,
@@ -49,7 +48,7 @@ export const sellInventory = asyncHandler(async (req, res) => {
       inventory,
       {
         $addToSet: { owners: { $each: ownersArray } },
-        $set: { status },
+        $set: { status, purchaseDate },
       },
       { new: true }
     );
@@ -58,14 +57,73 @@ export const sellInventory = asyncHandler(async (req, res) => {
   res.status(200).json(new ApiResponse(200, {}, "Inventory sold"));
 });
 
+// @route   GET /api/inventory/all
+// @desc    Get all inventories
+// @access  Private
+
+export const showInventories = asyncHandler(async (_, res) => {
+  const inventories = await Inventory.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "createdBy",
+        foreignField: "_id",
+        as: "createdBy",
+      },
+    },
+    {
+      $unwind: "$createdBy",
+    },
+    {
+      $project: {
+        inventoryType: 1,
+        floor: 1,
+        flatNo: 1,
+        status: 1,
+        purchaseDate: 1,
+        owners: 1,
+        createdBy: {
+          name: 1,
+          _id: 1,
+        },
+      },
+    },
+  ]);
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { inventories }, "Inventories found"));
+});
+
 // @route   GET /api/inventory/show-owners
 // @desc    Get all owners with their inventories
 // @access  Private
 export const showOwnersWithSpecificInventories = asyncHandler(
   async (_, res) => {
-    const owners = await Inventory.find()
-      .populate("owners", "name _id")
-      .select("owners");
+    const owners = await Inventory.aggregate([
+      {
+        $lookup: {
+          from: "owners", // Assuming 'owners' is the collection name
+          localField: "owners",
+          foreignField: "_id",
+          as: "ownersData",
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          owners: {
+            $map: {
+              input: "$ownersData",
+              as: "owner",
+              in: {
+                _id: "$$owner._id",
+                name: "$$owner.name",
+              },
+            },
+          },
+        },
+      },
+    ]);
 
     return res
       .status(200)
