0fa6cb0895c43b4153c3a7efa12710f9511324d4
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-21 16:35:45 +0500

    Refactor auth middleware to separate token verification and user retrieval

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,36 +3,33 @@ import asyncHandler from "../utils/AsyncHandler.js";
 import User from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 
-const auth = asyncHandler(async (req, _, next) => {
+const verifyToken = (token) => {
+  if (!token) {
+    throw new ApiError(401, "No token provided");
+  }
+
   try {
-    // Get the token from the request headers
-    const token = req.cookies.token;
-
-    if (!token) {
-      throw new ApiError(401, "No token provided");
-    }
-
-    // Verify the token
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
-    // Find the user by the decoded token id
-    const user = await User.findById(decoded.id).select(
-      "-password -refreshToken"
-    );
-
-    // If no user is found
-    if (!user) {
-      throw new ApiError(404, "Invalid access token");
-    }
-
-    // Attach the decoded user information to the request object
-    req.user = user;
-
-    // Call the next middleware or route handler
-    next();
+    return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
   } catch (error) {
     throw new ApiError(401, error?.message || "Invalid token");
   }
+};
+
+const findUser = async (id) => {
+  const user = await User.findById(id).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "Invalid access token");
+  }
+
+  return user;
+};
+
+const auth = asyncHandler(async (req, _, next) => {
+  const token = req.cookies.token;
+  const decoded = verifyToken(token);
+  req.user = await findUser(decoded.id);
+  next();
 });
 
 export default auth;
