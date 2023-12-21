6714b6c894859dd3d3a191af7a969c484569089b
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-21 16:35:25 +0500

    Refactor user controller and add new routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -41,11 +41,13 @@ export const login = asyncHandler(async (req, res) => {
   // Check if password matches
   const isMatch = await user.matchPassword(password);
   if (!isMatch) {
-    throw new ApiError(400, "Invalid credentials");
+    res.status(400).json(new ApiResponse(400, {}, "Invalid credentials"));
   }
 
   // Generate tokens
-  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
 
   // Get the logged in user
   const loggedInUser = await User.findById(user._id).select(
@@ -68,90 +70,12 @@ export const login = asyncHandler(async (req, res) => {
     );
 });
 
-// @route   UPDATE /api/user/:id/update
-// @desc    Update user
+// @route   GET /api/user/current-user
+// @desc    Get current user
 // @access  Private
-export const updateUser = asyncHandler(async (req, res) => {
-  // Get the user input
-  const { name, email, password, role } = req.body;
-
-  // Get the user id
-  const { id } = req.params;
-
-  // Check if user exists
-  const user = await User.findById(id);
-  if (!user) {
-    throw new ApiError(404, "User not found");
-  }
-
-  // Update user
-  if (name) {
-    user.name = name;
-  }
-
-  if (email) {
-    user.email = email;
-  }
-
-  if (password) {
-    user.password = password;
-  }
-
-  if (role) {
-    user.role = role;
-  }
-
-  await user.save();
-
-  return res.status(200).json(new ApiResponse(200, {}, "User updated"));
-});
-
-// @route   GET /api/user/show-users
-// @desc    Get all users
-// @access  Private
-export const showUsers = asyncHandler(async (_, res) => {
-  // Get all users
-  const users = await User.find({}).select("-password -refreshToken");
-  return res.status(200).json(new ApiResponse(200, { users }, "Users found"));
-});
-
-// @route   POST /api/user/refresh-token
-// @desc    Refresh token
-// @access  Private
-export const tokenRefresh = asyncHandler(async (req, res) => {
-  // Get the refresh token from the request cookies
-  const { refreshToken } = req.cookies;
-  if (!refreshToken) {
-    throw new ApiError(401, "No refresh token provided");
-  }
-
-  // Verify the refresh token
-  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-
-  // Find the user by the decoded token id
-  const user = await User.findById(decoded.id);
-
-  // If no user is found
-  if (!user) {
-    throw new ApiError(404, "Invalid refresh token");
-  }
-
-  // Generate new tokens
-  const { accessToken, refreshToken: newRefreshToken } =
-    generateAccessAndRefreshToken(user._id);
-
-  // Attach the new tokens to the response cookie
-  const options = {
-    expires: new Date(Date.now() + 86400000), // 24 hours
-    httpOnly: true,
-    secure: process.env.SECURE || false, // set to true if your using https
-  };
-
-  return res
-    .status(200)
-    .cookie("token", accessToken, options)
-    .cookie("refreshToken", newRefreshToken, options)
-    .json(new ApiResponse(200, {}, "Token refreshed successfully"));
+export const currentUser = asyncHandler((req, res) => {
+  const { user } = req;
+  return res.status(200).json({ user });
 });
 
 // @route   POST /api/user/logout
@@ -178,6 +102,149 @@ export const userLogout = asyncHandler(async (req, res) => {
     .clearCookie("token", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "Logged out successfully"));
+});
+
+// @route   POST /api/user/refresh-token
+// @desc    Refresh token
+// @access  Public
+export const tokenRefresh = asyncHandler(async (req, res) => {
+  // Get the refresh token from the request cookies
+  const { refreshToken } = req.cookies;
+  if (!refreshToken) {
+    throw new ApiError(401, "No refresh token provided");
+  }
+
+  // Verify the refresh token
+  const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+  // Find the user by the decoded token id
+  const user = await User.findById(decoded.id);
+
+  // If no user is found
+  if (!user) {
+    throw new ApiError(404, "Invalid refresh token");
+  }
+
+  // Generate new tokens
+  const { accessToken, refreshToken: newRefreshToken } =
+    await generateAccessAndRefreshToken(user._id);
+
+  // Attach the new tokens to the response cookie
+  const options = {
+    expires: new Date(Date.now() + 86400000), // 24 hours
+    httpOnly: true,
+    secure: process.env.SECURE || false, // set to true if your using https
+  };
+
+  return res
+    .status(200)
+    .cookie("token", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(new ApiResponse(200, {}, "Token refreshed successfully"));
+});
+
+// @route   PUT /api/user/:id/update
+// @desc    Update user
+// @access  Private
+export const updateUser = asyncHandler(async (req, res) => {
+  // Get the user id
+  const { id } = req.params;
+
+  // Get the user input
+  const { name, email, role } = req.body;
+
+  // Check if user exists
+  const user = await User.findById(id).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // Update the user
+  user.name = name || user.name;
+  user.email = email || user.email;
+  user.role = role || user.role;
+
+  await user.save();
+
+  return res.status(200).json(new ApiResponse(200, { user }, "User updated"));
+});
+
+// @route   PUT /api/user/change-password
+// @desc    Change password
+// @access  Private
+export const changePassword = asyncHandler(async (req, res) => {
+  // Get the user input
+  const { currentPassword, newPassword } = req.body;
+
+  // Get the user id
+  const { _id: id } = req.user;
+
+  // Check if user exists
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // Check if current password matches
+  const isMatch = await user.matchPassword(currentPassword);
+  if (!isMatch) {
+    throw new ApiError(400, "Invalid credentials");
+  }
+
+  // Update the password
+  user.password = newPassword;
+
+  await user.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"));
+});
+
+// @route   GET /api/user
+// @desc    Get all users
+// @access  Private
+export const showUsers = asyncHandler(async (_, res) => {
+  // Get all users
+  const users = await User.find({}).select("-password -refreshToken");
+  return res.status(200).json(new ApiResponse(200, { users }, "Users found"));
+});
+
+// @route  GET /api/user/:id
+// @desc   Get user by id
+// @access Private
+export const getUserById = asyncHandler(async (req, res) => {
+  // Get the user id
+  const { id } = req.params;
+
+  // Check if user exists
+  const user = await User.findById(id).select("-password -refreshToken");
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return res.status(200).json(new ApiResponse(200, { user }, "User found"));
+});
+
+// @route   DELETE /api/user/:id/delete
+// @desc    Delete user
+// @access  Private
+export const deleteUser = asyncHandler(async (req, res) => {
+  // Get the user id
+  const { id } = req.params;
+
+  // Check if user exists
+  const user = await User.findById(id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  await user.deleteOne();
+
+  return res.status(200).json(new ApiResponse(200, {}, "User deleted"));
 });
 
 // Private Functions
