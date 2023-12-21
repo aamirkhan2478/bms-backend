92a9494f591e2e0e94fb03b361f07a09862812c9
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-21 17:35:15 +0500

    Add validation for purchase date in sellInventorySchema

--- src/validator/sell_inventory.validator.js
+++ src/validator/sell_inventory.validator.js
@@ -5,6 +5,20 @@ const sellInventorySchema = z.object({
     z.string({ required_error: "Inventory ID is required!" })
   ),
   owners: z.array(z.string({ required_error: "Owner ID is required!" })),
+  purchaseDate: z
+    .string({ required_error: "Purchase date is required!" })
+    .refine((value) => {
+      const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+
+      if (!dateRegex.test(value)) {
+        return {
+          success: false,
+          message: "Invalid date format. Please use YYYY-MM-DD.",
+        };
+      }
+
+      return true;
+    }),
 });
 
 export default sellInventorySchema;
