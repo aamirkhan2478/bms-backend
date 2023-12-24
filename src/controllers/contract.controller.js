94ac627cf62e7f0eee85924f8db33fd666f28b62
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-25 00:08:13 +0500

    Add OwnerSignContract, TenantSignContract, and RentalInventory models to contract.controller.js

--- src/controllers/contract.controller.js
+++ src/controllers/contract.controller.js
@@ -1,22 +1,23 @@
 import mongoose from "mongoose";
 import Contract from "../models/contract.model.js";
 import Inventory from "../models/inventory.model.js";
-import Owner from "../models/owner.model.js";
-import Tenant from "../models/tenant.model.js";
 import mappingArray from "../utils/mapping_arrays.utils.js";
 import fileArray from "../utils/upload_images.utils.js";
 import asyncHandler from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import OwnerSignContract from "../models/owner_sign_contract.model.js";
+import TenantSignContract from "../models/tenant_sign_contract.model.js";
+import RentalInventory from "../models/rental_inventory.model.js";
 
 // @route   POST /api/contract/add
 // @desc    Add new contract
 // @access  Private
 export const addContract = asyncHandler(async (req, res) => {
   const {
-    tenants,
-    inventory,
     owners,
+    inventory,
+    tenants,
     signingDate,
     startDate,
     endDate,
@@ -41,21 +42,20 @@ export const addContract = asyncHandler(async (req, res) => {
 
   const ownerArray = mappingArray(owners);
   const tenantArray = mappingArray(tenants);
-
-  // Check inventories ObjectId is valid
+  // Check inventory ObjectId is valid
   if (!mongoose.Types.ObjectId.isValid(inventory)) {
     throw new ApiError(400, "Invalid inventory id");
   }
 
   // Check owners ObjectId is valid
-  for (const owner of ownerArray) {
+  for (let owner of ownerArray) {
     if (!mongoose.Types.ObjectId.isValid(owner)) {
       throw new ApiError(400, "Invalid owner id");
     }
   }
 
   // Check tenants ObjectId is valid
-  for (const tenant of tenantArray) {
+  for (let tenant of tenantArray) {
     if (!mongoose.Types.ObjectId.isValid(tenant)) {
       throw new ApiError(400, "Invalid tenant id");
     }
@@ -75,9 +75,7 @@ export const addContract = asyncHandler(async (req, res) => {
   const imagesArray = await fileArray(files, basePath);
 
   const contract = new Contract({
-    tenants: tenantArray,
     inventory,
-    owners: ownerArray,
     signingDate,
     startDate,
     endDate,
@@ -108,39 +106,33 @@ export const addContract = asyncHandler(async (req, res) => {
   await Inventory.findByIdAndUpdate(
     inventory,
     {
-      $addToSet: { contracts: createdContract._id, tenants: tenantArray },
       $set: { status: updatedInventoryStatus },
     },
     { new: true }
   );
 
-  // Update owner and tenant contracts
-  for (const owner of ownerArray) {
-    await Owner.findByIdAndUpdate(
-      owner,
-      {
-        $addToSet: { contracts: createdContract._id },
-      },
-      { new: true }
-    );
-  }
+  // Save signed contract to owners and tenants
+  await OwnerSignContract.insertMany(
+    owners.map((owner) => ({
+      ownerId: owner,
+      contractId: createdContract._id,
+    }))
+  );
 
-  for (const tenant of tenantArray) {
-    await Tenant.findByIdAndUpdate(
-      tenant,
-      {
-        $addToSet: {
-          contracts: createdContract._id,
-          inventories: inventory,
-        },
-      },
-      { new: true }
-    );
-  }
+  await TenantSignContract.insertMany(
+    tenants.map((tenant) => ({
+      tenantId: tenant,
+      contractId: createdContract._id,
+    }))
+  );
 
-  res
-    .status(201)
-    .json(
-      new ApiResponse(200, { contract: createdContract }, "Contract added")
-    );
+  // Save tenants and inventory to rental inventory
+  await RentalInventory.insertMany(
+    tenants.map((tenant) => ({
+      tenantId: tenant,
+      inventoryId: inventory,
+    }))
+  );
+
+  res.status(201).json(new ApiResponse(200, {}, "Contract added"));
 });
