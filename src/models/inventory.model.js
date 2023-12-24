80b5dd658adc93f72e6338fbebe6c42a4ab2a1ab
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-25 00:08:48 +0500

    Refactor inventory model schema

--- src/models/inventory.model.js
+++ src/models/inventory.model.js
@@ -14,34 +14,6 @@ const inventorySchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    owners: [
-      {
-        _id: false,
-        owner: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Owner",
-          default: null,
-        },
-        purchaseDate: {
-          type: Date,
-          default: null,
-        },
-      },
-    ],
-    tenants: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Tenant",
-        default: null,
-      },
-    ],
-    contracts: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Contract",
-        default: null,
-      },
-    ],
     status: {
       type: String,
       enum: ["for sell", "sold", "rented"],
