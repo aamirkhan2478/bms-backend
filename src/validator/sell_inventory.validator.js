bafdc5feb99f04ac231a80c7c2a24ea31252f890
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-25 00:10:17 +0500

    Refactor sellInventorySchema in sell_inventory.validator.js

--- src/validator/sell_inventory.validator.js
+++ src/validator/sell_inventory.validator.js
@@ -1,52 +1,21 @@
 import { z } from "zod";
 
 const sellInventorySchema = z.object({
-  inventories: z
-    .array(
-      z
-        .object({
-          inventory: z.string({ required_error: "Inventory ID is required!" }),
-          purchaseDate: z
-            .string({ required_error: "Purchase date is required!" })
-            .refine((value) => {
-              const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  ownerId: z.string({ required_error: "Owner is required" }),
+  inventoryId: z.string({ required_error: "inventory is required" }),
+  purchaseDate: z
+    .string({ required_error: "Purchase Date is required" })
+    .refine((value) => {
+      const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-              if (!dateRegex.test(value)) {
-                return {
-                  success: false,
-                  message: "Invalid date format. Please use YYYY-MM-DD.",
-                };
-              }
-
-              return true;
-            }),
-        })
-        .required({ message: "Inventory is required!" })
-    )
-    .nonempty({ message: "At least one inventory is required!" }),
-  owners: z
-    .array(
-      z
-        .object({
-          owner: z.string({ required_error: "Owner ID is required!" }),
-          purchaseDate: z
-            .string({ required_error: "Purchase date is required!" })
-            .refine((value) => {
-              const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-
-              if (!dateRegex.test(value)) {
-                return {
-                  success: false,
-                  message: "Invalid date format. Please use YYYY-MM-DD.",
-                };
-              }
-
-              return true;
-            }),
-        })
-        .required({ message: "Owner is required!" })
-    )
-    .nonempty({ message: "At least one owner is required!" }),
+      if (!dateRegex.test(value)) {
+        return {
+          success: false,
+          message: "Invalid date format. Please use YYYY-MM-DD.",
+        };
+      }
+      return true;
+    }),
 });
 
 export default sellInventorySchema;
