533c7a869b5cf16259e7ab37b7381f57d3e122ae
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-27 00:23:37 +0500

    Add inventory search and pagination logic

--- src/controllers/inventory.controller.js
+++ src/controllers/inventory.controller.js
@@ -1,9 +1,8 @@
+import mongoose from "mongoose";
 import Inventory from "../models/inventory.model.js";
-import Owner from "../models/owner.model.js";
 import SellInventory from "../models/sell_inventory.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/AsyncHandler.js";
-import mappingArray from "../utils/mapping_arrays.utils.js";
 
 // @route   POST /api/inventory/add
 // @desc    Add new inventory
@@ -47,8 +46,25 @@ export const sellInventory = asyncHandler(async (req, res) => {
 // @route   GET /api/inventory/all
 // @desc    Get all inventories
 // @access  Private
+export const showInventories = asyncHandler(async (req, res) => {
+  const { search, page = 1, limit = 3 } = req.query;
 
-export const showInventories = asyncHandler(async (_, res) => {
+  // Define the match object for search
+  const match = {};
+  if (search) {
+    match.$or = [
+      { inventoryType: { $regex: search, $options: "i" } },
+      { floor: { $regex: search, $options: "i" } },
+      { flatNo: { $regex: search, $options: "i" } },
+      { status: { $regex: search, $options: "i" } },
+      { createdBy: { $regex: search, $options: "i" } },
+    ];
+  }
+
+  // Pagination logic
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
   const inventories = await Inventory.aggregate([
     {
       $lookup: {
@@ -62,23 +78,116 @@ export const showInventories = asyncHandler(async (_, res) => {
       $unwind: "$createdBy",
     },
     {
+      $addFields: {
+        createdBy: "$createdBy.name",
+      },
+    },
+    {
+      $match: match, // Add the match object for search
+    },
+    {
       $project: {
         inventoryType: 1,
         floor: 1,
         flatNo: 1,
         status: 1,
-        purchaseDate: 1,
-        owners: 1,
-        createdBy: {
-          name: 1,
-          _id: 1,
-        },
+        createdBy: 1,
+        createdAt: 1,
+      },
+    },
+    {
+      $skip: startIndex, // Pagination: Skip documents
+    },
+    {
+      $limit: limit, // Pagination: Limit documents per page
+    },
+  ]);
+
+  // Pagination result
+  const pagination = {};
+  if (endIndex < (await Inventory.countDocuments().exec())) {
+    pagination.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, { inventories, pagination }, "Inventories found")
+    );
+});
+
+// @route   GET /api/inventory/:id
+// @desc    Get inventory details
+// @access  Private
+export const showInventory = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const inventory = await Inventory.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(id),
+      },
+    },
+    {
+      $lookup: {
+        from: "sellinventories",
+        localField: "_id",
+        foreignField: "inventoryId",
+        as: "sellInventory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "owners",
+              localField: "ownerId",
+              foreignField: "_id",
+              as: "owners",
+            },
+          },
+          {
+            $unwind: "$owners",
+          },
+          {
+            $addFields: {
+              ownerName: "$owners.name",
+            },
+          },
+          {
+            $project: {
+              _id: 1,
+              purchaseDate: 1,
+              ownerName: 1,
+              isActive: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        inventoryType: 1,
+        floor: 1,
+        flatNo: 1,
+        status: 1,
+        "sellInventory._id": 1,
+        "sellInventory.purchaseDate": 1,
+        "sellInventory.ownerName": 1,
+        "sellInventory.ownerName": 1,
+        "sellInventory.isActive": 1,
       },
     },
   ]);
   return res
     .status(200)
-    .json(new ApiResponse(200, { inventories }, "Inventories found"));
+    .json(new ApiResponse(200, { inventory: inventory[0] }, "Inventory found"));
 });
 
 // @route   GET /api/inventory/show-inventories-with-owners
