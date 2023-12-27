68b7b8d02eb347b83a1e4e49c44c14a48f51af76
aamirkhan2478 <aamir.khan2478@gmail.com>
2023-12-28 00:24:18 +0500

    Refactor auth.middleware.js to use ApiResponse instead of ApiError

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,34 +1,37 @@
 import jwt from "jsonwebtoken";
 import asyncHandler from "../utils/AsyncHandler.js";
 import User from "../models/user.model.js";
-import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+// import { ApiError } from "../utils/ApiError.js";
 
-const verifyToken = (token) => {
+const verifyToken = (res, token) => {
   if (!token) {
-    throw new ApiError(401, "No token provided");
+    res.status(401).json(new ApiResponse(401, {}, "No token provided"));
   }
 
   try {
     return jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
   } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid token");
+    res
+      .status(401)
+      .json(new ApiResponse(401, {}, error?.message || "Invalid token"));
   }
 };
 
-const findUser = async (id) => {
+const findUser = async (res, id) => {
   const user = await User.findById(id).select("-password -refreshToken");
 
   if (!user) {
-    throw new ApiError(404, "Invalid access token");
+    res.status(404).json(new ApiResponse(404, {}, "Invalid access token"));
   }
 
   return user;
 };
 
-const auth = asyncHandler(async (req, _, next) => {
+const auth = asyncHandler(async (req, res, next) => {
   const token = req.cookies.token;
-  const decoded = verifyToken(token);
-  req.user = await findUser(decoded.id);
+  const decoded = verifyToken(res, token);
+  req.user = await findUser(res, decoded.id);
   next();
 });
 
