9ee00a308a552fba87b82668ee9da48c78a3a90c
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-02 14:34:10 +0500

    Add owner search and pagination functionality

--- src/controllers/owner.controller.js
+++ src/controllers/owner.controller.js
@@ -3,6 +3,7 @@ import mappingArray from "../utils/mapping_arrays.utils.js";
 import fileArray from "../utils/upload_images.utils.js";
 import asyncHandler from "../utils/AsyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import mongoose from "mongoose";
 // import { ApiError } from "../utils/ApiError.js";
 
 // @route   POST /api/owner/add
@@ -38,12 +39,6 @@ export const addOwner = asyncHandler(async (req, res) => {
 
   const imagesArray = await fileArray(files, basePath);
 
-  // Check if email already exists
-  const emailExist = await Owner.findOne({ email });
-  if (emailExist) {
-    res.status(400).json(new ApiResponse(400, {}, "Email already exists"));
-  }
-
   // Check if CNIC already exists
   const cnicExist = await Owner.findOne({ cnic });
   if (cnicExist) {
@@ -75,7 +70,26 @@ export const addOwner = asyncHandler(async (req, res) => {
 // @route   GET /api/owner/all
 // @desc    Get all owners
 // @access  Private
-export const showOwners = asyncHandler(async (_, res) => {
+export const showOwners = asyncHandler(async (req, res) => {
+  const { search, page = 1, limit = 5 } = req.query;
+
+  // Define the match object for search
+  const match = {};
+  if (search) {
+    match.$or = [
+      { name: { $regex: search, $options: "i" } },
+      { father: { $regex: search, $options: "i" } },
+      { cnic: { $regex: search, $options: "i" } },
+      { phoneNumber: { $regex: search, $options: "i" } },
+      { whatsapp: { $regex: search, $options: "i" } },
+      { createdBy: { $regex: search, $options: "i" } },
+    ];
+  }
+
+  // Pagination logic
+  const startIndex = (page - 1) * parseInt(limit);
+  const endIndex = page * parseInt(limit);
+
   const owners = await Owner.aggregate([
     {
       $lookup: {
@@ -87,6 +101,115 @@ export const showOwners = asyncHandler(async (_, res) => {
     },
     {
       $unwind: "$createdBy",
+    },
+    {
+      $addFields: {
+        createdBy: "$createdBy.name",
+      },
+    },
+    {
+      $match: match, // Add the match object for search
+    },
+    {
+      $project: {
+        name: 1,
+        father: 1,
+        cnic: 1,
+        phoneNumber: 1,
+        whatsapp: 1,
+        createdBy: 1,
+        createdAt: 1,
+      },
+    },
+    {
+      $skip: startIndex, // Pagination: Skip documents
+    },
+    {
+      $limit: parseInt(limit), // Pagination: Limit documents per page
+    },
+  ]);
+
+  const total = await Owner.find(match).count();
+
+  // Pagination result
+  const pagination = {};
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit: parseInt(limit),
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit: parseInt(limit),
+    };
+  }
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { owners, totalOwners: total, pagination },
+        "Owners found"
+      )
+    );
+});
+
+// @route   GET /api/owner/:id
+// @desc    Get owner by id
+// @access  Private
+export const showOwner = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const owner = await Owner.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(id),
+      },
+    },
+    {
+      $lookup: {
+        from: "sellinventories",
+        localField: "_id",
+        foreignField: "ownerId",
+        as: "inventories",
+        pipeline: [
+          {
+            $lookup: {
+              from: "inventories",
+              localField: "inventoryId",
+              foreignField: "_id",
+              as: "inventory",
+            },
+          },
+          {
+            $unwind: "$inventory",
+          },
+          {
+            $addFields: {
+              inventoryType: "$inventory.inventoryType",
+              floor: "$inventory.floor",
+              flatNo: "$inventory.flatNo",
+            },
+          },
+          {
+            $project: {
+              _id: 1,
+              inventoryType: 1,
+              floor: 1,
+              flatNo: 1,
+              purchaseDate: 1,
+              isActive: 1,
+            },
+          },
+          {
+            $match: {
+              isActive: true,
+            },
+          },
+        ],
+      },
     },
     {
       $project: {
@@ -103,14 +226,114 @@ export const showOwners = asyncHandler(async (_, res) => {
         jobTitle: 1,
         jobLocation: 1,
         jobOrganization: 1,
+        bankName: 1,
+        bankTitle: 1,
+        bankBranchAddress: 1,
+        bankAccountNumber: 1,
+        bankIbnNumber: 1,
         images: 1,
-        inventories: 1,
-        createdBy: {
-          name: 1,
-          _id: 1,
-        },
+        remarks: 1,
+        "inventories._id": 1,
+        "inventories.inventoryType": 1,
+        "inventories.floor": 1,
+        "inventories.flatNo": 1,
+        "inventories.purchaseDate": 1,
+        "inventories.isActive": 1,
       },
     },
   ]);
-  return res.status(200).json(new ApiResponse(200, { owners }, "Owners found"));
+
+  if (!owner) {
+    res.status(404).json(new ApiResponse(404, {}, "Owner not found"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { owner: owner[0] }, "Owner found"));
+});
+
+// @route   PATCH /api/owner/:id/update-images
+// @desc    Update owner images
+// @access  Private
+export const updateImages = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const files = req.files;
+  const basePath = `${req.protocol}://${req.get("host")}/uploads/`;
+  const imagesArray = await fileArray(files, basePath);
+
+  const owner = await Owner.findById(id);
+  if (!owner) {
+    res.status(404).json(new ApiResponse(404, {}, "Owner not found"));
+  }
+
+  owner.images = imagesArray;
+  await owner.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { owner }, "Owner images updated"));
+});
+
+// @route   PATCH /api/owner/:id/update
+// @desc    Update owner
+// @access  Private
+export const updateOwner = asyncHandler(async (req, res) => {
+  const {
+    name,
+    father,
+    cnic,
+    cnicExpiry,
+    phoneNumber,
+    emergencyNumber,
+    whatsapp,
+    email,
+    currentAddress,
+    permanentAddress,
+    jobTitle,
+    jobLocation,
+    jobOrganization,
+    bankName,
+    bankTitle,
+    bankBranchAddress,
+    bankAccountNumber,
+    bankIbnNumber,
+    remarks,
+  } = req.body;
+
+  const { id } = req.params;
+
+  const phoneNumbers = mappingArray(phoneNumber);
+  const emergencyNumbers = mappingArray(emergencyNumber);
+  const whatsappNumbers = mappingArray(whatsapp);
+
+  const owner = await Owner.findById(id);
+  if (!owner) {
+    res.status(404).json(new ApiResponse(404, {}, "Owner not found"));
+  }
+
+  owner.name = name;
+  owner.father = father;
+  owner.cnic = cnic;
+  owner.cnicExpiry = cnicExpiry;
+  owner.phoneNumber = phoneNumbers;
+  owner.emergencyNumber = emergencyNumbers;
+  owner.whatsapp = whatsappNumbers;
+  owner.email = email;
+  owner.currentAddress = currentAddress;
+  owner.permanentAddress = permanentAddress;
+  owner.jobTitle = jobTitle;
+  owner.jobLocation = jobLocation;
+  owner.jobOrganization = jobOrganization;
+  owner.bankName = bankName;
+  owner.bankTitle = bankTitle;
+  owner.bankBranchAddress = bankBranchAddress;
+  owner.bankAccountNumber = bankAccountNumber;
+  owner.bankIbnNumber = bankIbnNumber;
+  owner.remarks = remarks;
+
+  await owner.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { owner }, "Owner updated"));
 });
