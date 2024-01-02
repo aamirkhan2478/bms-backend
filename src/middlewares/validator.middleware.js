867f174e31fbd94e78397dad8cdee01a51f9002c
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-02 14:35:25 +0500

    Commented out import of ApiError and added import of ApiResponse in validator.middleware.js

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,4 +1,5 @@
-import { ApiError } from "../utils/ApiError.js";
+// import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import asyncHandler from "../utils/AsyncHandler.js";
 
 const validatorMiddleware = (schema) =>
