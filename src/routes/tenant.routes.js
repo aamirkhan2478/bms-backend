a33a53d1228586fc8fba37c740960f9c4e19a7c4
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-03 10:58:33 +0500

    Add routes for showing a specific tenant and updating tenant images and information

--- src/routes/tenant.routes.js
+++ src/routes/tenant.routes.js
@@ -1,6 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { addTenant, showTenants } from "../controllers/tenant.controller.js";
+import {
+  addTenant,
+  showTenants,
+  showTenant,
+  updateImages,
+  updateTenant,
+} from "../controllers/tenant.controller.js";
 import validatorMiddleware from "../middlewares/validator.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 import ownerTenantSchema from "../validator/owner_tenant.validator.js";
@@ -13,5 +19,12 @@ router
     addTenant
   );
 router.route("/all").get(showTenants);
+router.route("/:id").get(showTenant);
+router
+  .route("/:id/update-images")
+  .patch(upload.array("images", 10), updateImages);
+router
+  .route("/:id/update")
+  .patch(validatorMiddleware(ownerTenantSchema), updateTenant);
 
 export default router;
