7b128a2587e672d8ae7b6dd3a7eccab6beebf00e
aamirkhan2478 <aamir.khan2478@gmail.com>
2024-01-05 11:19:45 +0500

    Add new route for expired CNIC check

--- src/routes/tenant.routes.js
+++ src/routes/tenant.routes.js
@@ -6,6 +6,7 @@ import {
   showTenant,
   updateImages,
   updateTenant,
+  expiredCnic,
 } from "../controllers/tenant.controller.js";
 import validatorMiddleware from "../middlewares/validator.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
@@ -19,12 +20,13 @@ router
     addTenant
   );
 router.route("/all").get(showTenants);
-router.route("/:id").get(showTenant);
+router.route("/:id/show").get(showTenant);
 router
   .route("/:id/update-images")
   .patch(upload.array("images", 10), updateImages);
 router
   .route("/:id/update")
   .patch(validatorMiddleware(ownerTenantSchema), updateTenant);
+router.route("/expired-cnic").get(expiredCnic);
 
 export default router;
